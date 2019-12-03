8a65fbb417f1a87ed5df10e73142de442ba3f5fd
NguyenBaThuan <nguyenbathuan98@gmail.com>
2019-12-03 15:10:55 +0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace SerialPort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ptb2 = new System.Windows.Forms.PictureBox();
+            this.ptb1 = new System.Windows.Forms.PictureBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbCompare = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptb2
+            // 
+            this.ptb2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ptb2.Location = new System.Drawing.Point(26, 54);
+            this.ptb2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ptb2.Name = "ptb2";
+            this.ptb2.Size = new System.Drawing.Size(166, 232);
+            this.ptb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb2.TabIndex = 5;
+            this.ptb2.TabStop = false;
+            // 
+            // ptb1
+            // 
+            this.ptb1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ptb1.Location = new System.Drawing.Point(212, 54);
+            this.ptb1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ptb1.Name = "ptb1";
+            this.ptb1.Size = new System.Drawing.Size(166, 232);
+            this.ptb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptb1.TabIndex = 6;
+            this.ptb1.TabStop = false;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.Path = "C:\\out";
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            this.fileSystemWatcher1.Changed += new System.IO.FileSystemEventHandler(this.fileSystemWatcher1_Changed);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Ảnh hiện tại";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 28);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ảnh trước";
+            // 
+            // lb
+            // 
+            this.lb.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lb.FormattingEnabled = true;
+            this.lb.Location = new System.Drawing.Point(408, 54);
+            this.lb.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(115, 316);
+            this.lb.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(430, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Danh sách ảnh";
+            // 
+            // lbCompare
+            // 
+            this.lbCompare.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lbCompare.FormattingEnabled = true;
+            this.lbCompare.Location = new System.Drawing.Point(541, 54);
+            this.lbCompare.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lbCompare.Name = "lbCompare";
+            this.lbCompare.Size = new System.Drawing.Size(438, 316);
+            this.lbCompare.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(578, 28);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Phần trăm giống nhau";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 405);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbCompare);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ptb1);
+            this.Controls.Add(this.ptb2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "s";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox ptb2;
+        private System.Windows.Forms.PictureBox ptb1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lbCompare;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace SerialPort
+{
+    public partial class Form1 : Form
+    {
+
+
+
+        private List<string> listPicture;
+        private List<string> listResult;
+        public Form1()
+        {
+            InitializeComponent();
+            listPicture = new List<string>();
+            listResult = new List<string>();
+        }
+
+        private void fileSystemWatcher1_Changed(object sender, FileSystemEventArgs e)
+        {
+            ptb1.Image = Image.FromFile(e.FullPath);
+            listPicture.Add(e.FullPath);
+            lb.Items.Add(listPicture[listPicture.Count-1]);
+            if (listPicture.Count >= 2)
+            {
+                ptb2.Image = Image.FromFile(listPicture[listPicture.Count - 2]);
+                if (ptb2.Image != null)
+                {
+                    int kq = CompareTwoImage(ptb1.Image as Bitmap, ptb2.Image as Bitmap);
+                    double tile = (kq * (1.0) / 1024 * 100);
+                    tile = Math.Round(tile, 2);
+                    if (tile > 80)
+                    {
+                        listResult.Add("Tỉ lệ: " + tile + " - KQ: " + " Đứng yên" + " Time: " + DateTime.Now.ToString("hh:mm:ss"));
+                        //lbCompare.Items.Add("Tỉ lệ: " + tile +  " - KQ: " + " Đứng yên" +" Time: "+DateTime.Now.Second);
+                       
+                    }
+                    else
+                    {
+
+                        listResult.Add("Tỉ lệ: " + tile + " - KQ: " + " Chuyển động" + " Time: " + DateTime.Now.ToString("hh:mm:ss"));
+                        //lbCompare.Items.Add("Tỉ lệ: " + tile +  " - KQ: " + " Di chuyển" + " Time: " + DateTime.Now.Second);
+                    }
+                    var temp = listResult.AsEnumerable().Reverse();
+                    lbCompare.DataSource = temp.ToList();
+                    
+
+
+                }
+                
+                
+            }
+            
+            
+            
+        }
+        public static List<bool> GetHash(Bitmap bmpSource)
+        {
+            List<bool> lResult = new List<bool>();
+            //create new image with 16x16 pixel
+            Bitmap bmpMin = new Bitmap(bmpSource, new Size(32, 32));
+            for (int j = 0; j < bmpMin.Height; j++)
+            {
+                for (int i = 0; i < bmpMin.Width; i++)
+                {
+                    //reduce colors to true / false                
+                    lResult.Add(bmpMin.GetPixel(i, j).GetBrightness() < 0.25f);
+                }
+            }
+            return lResult;
+        }
+        public int CompareTwoImage(Bitmap bm1, Bitmap bm2)
+        {
+            int equalElements = 0;
+            List<bool> iHash1 = GetHash(bm1);
+            List<bool> iHash2 = GetHash(bm2);
+
+            //determine the number of equal pixel (x of 256)
+            equalElements = iHash1.Zip(iHash2, (i, j) => i == j).Count(eq => eq);
+            return equalElements;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kq = CompareTwoImage(ptb1.Image as Bitmap, ptb2.Image as Bitmap);
+            double tile = (kq * (1.0) / 1024 * 100);
+            tile = Math.Round(tile, 2);
+            if (tile > 80)
+            {
+                MessageBox.Show("Tỉ lệ: "+tile+" - Ảnh Hiện Tại: " + ptb1.Name + " - Ảnh trước: " + ptb2.Name + " - KQ: " + " Đứng yên");
+            }
+            else
+            {
+                MessageBox.Show("Tỉ lệ: "+tile+" - Ảnh Hiện Tại: " + ptb1.Name + " - Ảnh trước: " + ptb2.Name + " - KQ: " + " Chuyển động");
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ExecuteCommandAsync("java code.SimpleRead");
+        }
+
+        public void ExecuteCommandSync(object command)
+        {
+            try
+            {
+                // create the ProcessStartInfo using "cmd" as the program to be run,
+                // and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                // and then exit.
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+                procStartInfo.WorkingDirectory = @"C:\Program Files (x86)\Java\jdk1.8.0_74\bin";
+
+                // The following commands are needed to redirect the standard output.
+                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = true;
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = true;
+                // Now we create a process, assign its ProcessStartInfo and start it
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                // Get the output into a string
+                string result = proc.StandardOutput.ReadToEnd();
+                // Display the command output.
+                Console.WriteLine(result);
+            }
+            catch (Exception objException)
+            {
+                // Log the exception
+            }
+        }
+
+        public void ExecuteCommandAsync(string command)
+        {
+            try
+            {
+                //Asynchronously start the Thread to process the Execute command request.
+                Thread objThread = new Thread(new ParameterizedThreadStart(ExecuteCommandSync));
+                //Make the thread as background thread.
+                objThread.IsBackground = true;
+                //Set the Priority of the thread.
+                objThread.Priority = ThreadPriority.AboveNormal;
+                //Start the thread.
+                objThread.Start(command);
+            }
+            catch (ThreadStartException objException)
+            {
+                // Log the exception
+            }
+            catch (ThreadAbortException objException)
+            {
+                // Log the exception
+            }
+            catch (Exception objException)
+            {
+                // Log the exception
+            }
+        }
+    }
+}
+
